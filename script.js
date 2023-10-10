0ee8aa514e5cb998d1ed53b9736e9568492cb3cf
Stephanie Nwankwo <stephybelle208@gmail.com>
2023-10-10 14:43:17 +0100

    Updated.

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
-"use strict";
+'use strict';
 
-const modal = document.querySelector(".modal");
-const modal1 = document.querySelector(".modal1");
-const modal2 = document.querySelector(".modal2");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnCloseModal1 = document.querySelector(".close-modal1");
-const btnCloseModal2 = document.querySelector(".close-modal2");
-const btnOpenModal = document.querySelector(".show-modal");
-const btnOpenModal2 = document.querySelector(".show-modal2");
-const btnOpenModal3 = document.querySelector(".show-modal3");
-
+const modal = document.querySelector('.modal');
+const modal1 = document.querySelector('.modal1');
+const modal2 = document.querySelector('.modal2');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnCloseModal1 = document.querySelector('.close-modal1');
+const btnCloseModal2 = document.querySelector('.close-modal2');
+const btnOpenModal = document.querySelector('.show-modal');
+const btnOpenModal2 = document.querySelector('.show-modal2');
+const btnOpenModal3 = document.querySelector('.show-modal3');
 
 const openModal = function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
-btnOpenModal.addEventListener("click", openModal);
+btnOpenModal.addEventListener('click', openModal);
 
-btnOpenModal2.addEventListener("click", function () {
-    modal1.classList.remove("hidden");
-    overlay.classList.remove("hidden");
+btnOpenModal2.addEventListener('click', function () {
+  modal1.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 });
 
-btnOpenModal3.addEventListener("click", function () {
-    modal2.classList.remove("hidden");
-    overlay.classList.remove("hidden");
+btnOpenModal3.addEventListener('click', function () {
+  modal2.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 });
 
 const closeModal = function () {
-    modal.classList.add("hidden");
-    modal1.classList.add("hidden");
-    modal2.classList.add("hidden");
-    overlay.classList.add("hidden");
+  modal.classList.add('hidden');
+  modal1.classList.add('hidden');
+  modal2.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
 btnCloseModal.addEventListener('click', closeModal);
 
 btnCloseModal1.addEventListener('click', closeModal);
 
-btnCloseModal2.addEventListener('click', closeModal)
+btnCloseModal2.addEventListener('click', closeModal);
 
-overlay.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape') {
+    if (
+      !modal.classList.contains('hidden') ||
+      !modal1.classList.contains('hidden') ||
+      !modal2.classList.contains('hidden')
+    ) {
+      closeModal();
+    }
+  }
+});
